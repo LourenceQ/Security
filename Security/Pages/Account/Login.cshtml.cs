54faf1220ac017bee7c93f901d81522f4f86d137
LourenceQ <lqfmecatronica@gmail.com>
2022-05-25 16:44:19 -0300

    Removed History

---
+++ Security/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Security.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential{ get; set; }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            // verifying credential
+            if(Credential.UserName == "admin" && Credential.Password == "123456")
+            {
+                // creating security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@admin.com")
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+
+}
